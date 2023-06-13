8c1ccf815822d100bf2439940d6c4c4c80928c01
dark2phoenix <dark3phoenix@gmail.com>
2023-06-13 16:11:35 -0400

    Initial working solution

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -2,7 +2,9 @@
 set -o xtrace
 
 
-tdnf install ansible -y
+sudo apt install ansible-core -y
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
-ansible-playbook --connection=local configure-ubnutu.yml
+ANSIBLE_LOCALHOST_WARNING=False \
+ANSIBLE_INVENTORY_UNPARSED_WARNING=False \
+ansible-playbook --connection=local configure-ubuntu.yml
