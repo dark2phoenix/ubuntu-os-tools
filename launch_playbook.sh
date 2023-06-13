34825e34e76c31c070bd4c0b340a4d2da1ad4a93
dark2phoenix <dark3phoenix@gmail.com>
2023-06-13 15:01:45 +0000

    Initial porting of values

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -5,4 +5,4 @@ set -o xtrace
 tdnf install ansible -y
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
-ansible-playbook --connection=local configure-photon.yml
+ansible-playbook --connection=local configure-ubnutu.yml
