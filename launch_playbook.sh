5ca7b5a99deb425718e4eff4305114b3eb7fd4e6
dark2phoenix <dark3phoenix@gmail.com>
2023-06-14 14:37:48 -0400

    Fix some busgs: - Switch from ansible-core to ansible to avoid ansible-galaxy problems - Add "DEBIAN_FRONTEND=noninteractive" to prevent prompts during install

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -o xtrace
+#set -o xtrace
 
-
-sudo apt install ansible-core -y
-ansible-galaxy collection install community.general
-ansible-galaxy collection install ansible.posix
+DEBIAN_FRONTEND=noninteractive
+apt install -y python3-pip ansible
+ansible-galaxy collection install community.general --force
+ansible-galaxy collection install ansible.posix --force
 ANSIBLE_LOCALHOST_WARNING=False \
 ANSIBLE_INVENTORY_UNPARSED_WARNING=False \
 ansible-playbook --connection=local configure-ubuntu.yml
